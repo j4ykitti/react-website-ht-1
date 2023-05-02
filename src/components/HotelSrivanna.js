f1a513242b43069d3be616bcdd345240c90a6990
j4ykitti <antidotert@gmail.com>
2023-05-02 11:55:53 +0700

    test2

--- src/components/HotelSrivanna.js
+++ src/components/HotelSrivanna.js
@@ -1,5 +1,5 @@
 import './HotelSrivanna.css';
-import React from 'react';
+import React , {useState} from 'react';
 import '../App.css'
 import { FaChild } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
@@ -7,11 +7,19 @@ import { FaBed } from "react-icons/fa";
 import './Cards.css';
 import CartItem from './CartCardItem';
 import { IoAccessibility } from "react-icons/io5";
+import ReactDatePicker from 'react-datepicker';
 
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
 
 function HotelSrivanna(){
+    const [selectedDayRange, setSelectedDayRange] = useState({
+        from: null,
+        to: null
+    });
+   
     return (
-        <div>
+       <div>
             <div className="header" style = {{backgroundImage : "url(/images/hotel1.jpg"}} >
                  <h1> Srivanna Hotel</h1>
             </div>
@@ -21,18 +29,28 @@ function HotelSrivanna(){
             <li className='faci'>
             <p><FaCheck/>parking <FaCheck/>air-con <FaCheck/>wi-fi <FaCheck/>fitness <FaCheck/>airport-transfer</p>
             </li>
-            
-            <li className='type'>
+            <div className='calen'>
+            <Calendar
+                  value={selectedDayRange}
+                 onChange={setSelectedDayRange}
+                  shouldHighlightWeekends
+             />
+             </div>
+            <li className='type' >
             <CartItem
               src='images/Deluxe.jpg'
               text='Deluxe'
               label='1,500 THB'
               path='/hoteldetail'
+              
               />
+              
                 </li>
             <li className='detail'>
+                
                 <h3><IoAccessibility/> 1-2 adult <FaChild/> 0 kid <FaBed/> 1 QueenSize</h3>
             </li>
+            
 
             
             <li className='type'>
@@ -52,4 +70,4 @@ function HotelSrivanna(){
     )
 }
 
-export default HotelSrivanna
+export default HotelSrivanna 
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,13 +7,35 @@ const RegisterForm = () =>{
     return (
         <div className="cover">
             <h1>Register</h1>
+            <div className="box">
             <input type="text" placeholder="username" />
+            </div>
+            
+            <div className="box">
             <input type="password" placeholder="password" />
+            </div>
+            
+            <div className="box">
             <input type="con-password" placeholder="confirm password" />
+            </div>
+            
+            <div className="box">
+            <input type="user-email" placeholder="email" />
+            </div>
+            
+            <div className="box">
+            <input type="user-phone" placeholder="phone" />
+            </div>
             
             <div className="register-btn">
                 Register
                 </div>
+                <Link to='/sign-up' >
+              <p>
+                  Login
+              </p>
+              </Link>
+              
             
               
         </div>
